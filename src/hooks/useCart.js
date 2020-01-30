37277817e55c13f17723ea392f221ffd73ac268d
Joshua Rieth <joshua.m.rieth@gmail.com>
2020-01-30 17:50:41 -0500

    update removeItem function

--- src/hooks/useCart.js
+++ src/hooks/useCart.js
@@ -3,20 +3,14 @@ import { useLocalStorage } from './useLocalStorage';
 export const useCart = initialValue => {
 	const [cart, setCart] = useLocalStorage('cart', initialValue);
 
-		const addItem = item => {
+	const addItem = item => {
 		const cartID = Date.now();
 		const product = { ...item, cartID: cartID };
 		setCart([...cart, product]);
 	};
 
 	const removeItem = item => {
-		const newCart = [];
-		cart.map(product => {
-			if (product.cartID !== item) {
-				newCart.push(product);
-				console.log(product);
-			}
-		});
+		const newCart = cart.filter(item => item.cartID !== item);
 		setCart(newCart);
 	};
 
